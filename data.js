b8b9e34d8fa99a1af7980f358f5631497347cfaf
Algorithmic Anchor Admin <admin@algorithmicanchor.com>
2026-01-14 22:15:26 +0200

    Оновлення даних курсу через адмін-панель

--- data.js
+++ data.js
@@ -2,10 +2,10 @@
 const siteData = {
   "courses": [
     {
-      "id": "p",
-      "title": "Python",
-      "description": "Jdjdj",
-      "fullDescription": "Jdjdj",
+      "id": "python-junior-senior",
+      "title": "eeed",
+      "description": "eee",
+      "fullDescription": "eee",
       "image": "default-course.jpg",
       "lessonsCount": 0
     }
